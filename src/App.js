3e777fae155661a3a14c5ac61e5860dcc9811640
Cibin <134275527+CibinBK@users.noreply.github.com>
2023-06-20 15:10:28 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Login from './components/Login';
+import { Route, Routes } from 'react-router-dom';
+import AdminDash from './components/AdminDash';
+import Profile from './components/Profile';
+import Home from './components/Home';
+import SignUp from './components/SignUp';
+import UserDash from './components/UserDash';
+import Add from './components/Add';
 
 function App() {
+  const [user, setUser] = useState({});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login setUser={setUser}/>} />
+        <Route path="/admin" element={<AdminDash/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/user" element={<UserDash/>} />
+        <Route path="/add" element={<Add  userId={user.email}/>} />
+      </Routes>
     </div>
   );
 }
